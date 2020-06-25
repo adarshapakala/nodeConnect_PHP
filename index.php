3072b067a084dc7faa694de10eea741dbb9d1bca
Adarsh <38885821+adarshapakala@users.noreply.github.com>
2020-06-25 22:55:09 +0200

    User Account Creation And Login – Initial Files Created
    1. Dummy files for Wecome page, Create User account and Login page created
    2. Navigation between the pages added

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+//Welcome page
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> "Welcome Page"</title>
+	<h1>Wecome!</h1>
+
+</head>
+<body>
+	<button type="button" style = "background-color:green" onclick="window.location='http://localhost/protoType01/nodeConnect/login /loginPage.php'" class="Redirect" >Sign In </button>
+	<button type="button" style = "background-color:orange" onclick="window.location='http://localhost/protoType01/nodeConnect/login /createUserAccount.php'" class="Redirect" >Sign UP </button>
+</body>
+</html>
+
---
+++ login/loginPage.php
@@ -0,0 +1,15 @@
+<?php
+//Welcome page
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>log-in</title>
+	<h1> Log-in Page</h1>
+</head>
+<body>
+
+</body>
+</html>
