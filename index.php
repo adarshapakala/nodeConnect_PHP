de2c6f6f854db81c1812e8dad3e74c2761bed226
Adarsh <38885821+adarshapakala@users.noreply.github.com>
2020-06-26 12:17:23 +0200

    user account setting completed
    Following are completed:
    Create user
    Login using existing user

--- index.php
+++ index.php
@@ -1,19 +1,48 @@
-<?php
-//Welcome page
+<?php 
+	session_start(); 
 
+	if (!isset($_SESSION['username'])) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: registerUser/loginPage.php');
+	}
+
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: registerUser/loginPage.php");
+	}
 
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title> "Welcome Page"</title>
-	<h1>Wecome!</h1>
-
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="../styleSheets/style.css">
 </head>
 <body>
-	<button type="button" style = "background-color:green" onclick="window.location='http://localhost/protoType01/nodeConnect/login /loginPage.php'" class="Redirect" >Sign In </button>
-	<button type="button" style = "background-color:orange" onclick="window.location='http://localhost/protoType01/nodeConnect/login /createUserAccount.php'" class="Redirect" >Sign UP </button>
+	<div class="header">
+		<h2>Home Page</h2>
+	</div>
+	<div class="content">
+
+		<!-- notification message -->
+		<?php if (isset($_SESSION['success'])) : ?>
+			<div class="error success" >
+				<h3>
+					<?php 
+						echo $_SESSION['success']; 
+						unset($_SESSION['success']);
+					?>
+				</h3>
+			</div>
+		<?php endif ?>
+
+		<!-- logged in user information -->
+		<?php  if (isset($_SESSION['username'])) : ?>
+			<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+			<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+		<?php endif ?>
+	</div>
+		
 </body>
 </html>
-
--- registerUser/loginPage.php
+++ registerUser/loginPage.php
@@ -1,15 +1,36 @@
-<?php
-//Welcome page
-
-?>
-
+<?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>log-in</title>
-	<h1> Log-in Page</h1>
+	<title>Registration system PHP and MySQL</title>
+	<link rel="stylesheet" type="text/css" href="../styleSheets/style.css">
 </head>
 <body>
+
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+	
+	<form method="post" action="loginPage.php">
+
+		<?php include('../errorHandling/errors.php'); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="createUserAccount.php">Sign up</a>
+		</p>
+	</form>
+
 
 </body>
 </html>
